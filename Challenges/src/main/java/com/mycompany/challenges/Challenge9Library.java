23851bfefd66463cc33295bed1187709f281d8a9
Name <hillowoalde@gmail.com>
2023-07-27 11:58:13 -0400

    hi

--- Challenges/src/main/java/com/mycompany/challenges/Challenge9Library.java
+++ Challenges/src/main/java/com/mycompany/challenges/Challenge9Library.java
@@ -5,35 +5,48 @@
 package com.mycompany.challenges;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.List;
 /**
  *
  * @author Aidan Pereira
  */
 public class Challenge9Library {
     public static void main(String[] args) {
-        ArrayList<String> books = new ArrayList<>();
-        books.add("Shadow and Bone - Leigh Bardugo");
-        books.add("The Hunger Games - Suzanne Collins");
+        ArrayList<ArrayList<String>> books = new ArrayList<ArrayList<String>>((List.of(new ArrayList<String>(List.of("Shadow and Bone","Leigh Bardugo", "Young Adult Fantasy Adventure")),new ArrayList<String>(List.of("The Hunger Games","Suzanne Collins", "Dystopian Novel")))));
+        ArrayList<String> book = new ArrayList<>();
+        ArrayList<String> book2 = new ArrayList<>();
         Scanner scone = new Scanner(System.in);
+        String choices = "";
+        Integer choice = 0;
         while (true){
-            System.out.println("Hi, click 1 to see list of books 2 to withdraw 3 deposit 4 to escape simulation");
-            String choice = (scone.nextLine());
-            if (choice == "1"){
+            System.out.println("I, glick 1 do Z uh lizd ov bogs 2 widraw 3 debozit 4 ezgaybe zimuladion");
+            choice = Integer.parseInt(scone.nextLine());
+            if (choice == 1){
                 System.out.println(books);
             }
-            else if (choice == "2"){
+            else if (choice == 2){
                 System.out.println("wut bok?");
-                choice = (scone.nextLine());
+                choices = (scone.nextLine());
                 {
                     for (int counter = -1; counter < books.size(); counter++) { 		      
-                        if (books.get(counter).equals(choice)):
-                            System.out.println("wut bok?");
+                        if (books.get(counter).get(1).equals(choices)){
+                            System.out.println("daken.");
+                        }
                     }   	
                 }
             }
-            else if (choice == "3"){
+            else if (choice == 3){
+            System.out.println("wut bok?");
+                choices = (scone.nextLine());
+                {
+                    for (int counter = -1; counter < books.size(); counter++) { 		      
+                        if (books.get(counter).get(1).equals(choices)){
+                            System.out.println("geven.");
+                        }
+                    }   	
+                }
             }
-            else if (choice == "4"){
+            else if (choice == 4){
                 break;
             }
         }
